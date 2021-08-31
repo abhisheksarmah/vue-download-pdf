cb1eb029f464971b27370cd3fdca8d431a7b0b3d
Sarmah <abhishek.sarmah@capgemini.com>
2021-08-31 10:48:34 +0530

    POC for downloading pdf

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="printDownload">Print Download</button>
+    <Download v-show="false" ref="DownloadComp" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Download from './components/Download'
+  export default {
+    components: {
+      Download,
+    },
+    methods: {
+      printDownload () {
+        let w = window.open('','printwindow');
+        w.document.write(`<html>
+          <head>
+            <title>Print it!</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+            <style>
+              #print {background: red;}
+              .text-green { color: green;}
+              .text-bold {font-weight: bold;}
+              * {
+                -webkit-print-color-adjust: exact !important;   /* Chrome, Safari, Edge */
+                color-adjust: exact !important;                 /*Firefox*/
+              }
+            </style>
+          </head>
+          <body>`);
+        w.document.write(this.$refs.DownloadComp.$el.innerHTML);
+        w.document.write('</body></html>');
+        w.document.close();
+        w.setTimeout(function () {
+          w.print()
+          w.close()
+        }, 1000)
+      },
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
